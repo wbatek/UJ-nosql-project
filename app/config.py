04244c19727fa41e563019690d105a200860faef
wbatek <wojciech.batko02@gmail.com>
2025-05-22 20:46:18 +0200

    project set-up for FastAPI, React, MongoDB

--- app/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    MONGO_URI = os.environ.get("MONGO_URI", "mongodb://mongo:27017/mydatabase")
-    SECRET_KEY = os.environ.get("SECRET_KEY", "mysecretkey")
--- app/routes/main.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify
-from app import mongo
-
-main = Blueprint('main', __name__)
-
-@main.route('/')
-def index():
-    return jsonify({"message": "Welcome to the Flask API!"})
